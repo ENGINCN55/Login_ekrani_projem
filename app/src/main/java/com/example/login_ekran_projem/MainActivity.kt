0be2d7065571bf4bfc0e99e7b8a60716d6e76d7e
ENGİNCN55 <engincanculha55@gmail.com>
2023-06-24 22:07:12 +0300

    button'ları birbirlerine bağlayıp etkileşim sağladım

--- app/src/main/java/com/example/login_ekran_projem/MainActivity.kt
+++ app/src/main/java/com/example/login_ekran_projem/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.login_ekran_projem
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.example.login_ekran_projem.R.id.benimTextView2
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var editTextTextEmailAddress:TextView
+    private lateinit var buttonum: Button
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        editTextTextEmailAddress= findViewById(R.id.editTextTextEmailAddress)
+        buttonum=findViewById(R.id.buttonum)
+        buttonum.setOnClickListener {
+            editTextTextEmailAddress.text="merhaba bu benim bağlantılı metnim"
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/example/login_ekran_projem/yeniActivity.kt
@@ -0,0 +1,11 @@
+package com.example.login_ekran_projem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class yeniActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_yeni)
+    }
+}
