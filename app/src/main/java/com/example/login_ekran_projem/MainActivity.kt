f7c04dc212ac93cd38ae48229391a3237e2cc1a7
ENGİNCN55 <engincanculha55@gmail.com>
2023-06-27 18:30:34 +0300

    button'ları birbirlerine bağlayıp etkileşim sağladım

--- app/src/main/java/com/example/login_ekran_projem/MainActivity.kt
+++ app/src/main/java/com/example/login_ekran_projem/MainActivity.kt
@@ -5,20 +5,33 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import com.example.login_ekran_projem.R.id.benimTextView2
 
 class MainActivity : AppCompatActivity() {
     private lateinit var editTextTextEmailAddress:TextView
     private lateinit var buttonum: Button
+    private lateinit var yardim:TextView
+    private lateinit var buttonum2: Button
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        yardim= findViewById(R.id.yardim)
+        buttonum2=findViewById(R.id.buttonum2)
+        buttonum2.setOnClickListener {
+            yardim.text="buyrun"
+
+        }
+
         editTextTextEmailAddress= findViewById(R.id.editTextTextEmailAddress)
         buttonum=findViewById(R.id.buttonum)
         buttonum.setOnClickListener {
             editTextTextEmailAddress.text="merhaba bu benim bağlantılı metnim"
+
         }
+
+
 
 
     }
